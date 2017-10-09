c1da0ba0489f0454274970a6d8abaed946061b7e
Dan Leedham <danleedham@googlemail.com>
2017-10-09 10:08:05 +0100

    Add windups to index

--- src/index.php
+++ src/index.php
@@ -21,22 +21,28 @@
 				  </div>
 				  <div class="panel-body">
 					<div class="row">
-					  <div class="col-xs-6 col-sm-4">
+					  <div class="col-xs-6 col-md-3">
 						<a class="bootcards-summary-item" href="search.php">
 						  <img src="favicons/android-chrome-192x192.png" height="64" width="64">
-						  <h4>Member Search <span class="label label-info">650</span></h4>
+						  <h4>Member Search <span class="label label-info"><?php $house = "Commons"; include "template/int-numberofmembers.php" ?></span></h4>
 						</a>
 					  </div>
-					  <div class="col-xs-6 col-sm-4">
+					  <div class="col-xs-6 col-md-3">
 						<a class="bootcards-summary-item" href="qs.php">
 						  <img src="favicons/android-chrome-192x192.png" height="64" width="64">
 						  <h4>Question Stacker <span class="label label-danger"><?php echo date("Y-m-d");?></span></h4>
 						</a>
 					  </div>
-					  <div class="col-xs-6 col-sm-4">
+					  <div class="col-xs-6 col-md-3">
 						<a class="bootcards-summary-item" href="who.php">
 						  <img src="favicons/android-chrome-192x192.png" height="64" width="64">
-						  <h4>Guess Who? <span class="label label-warning">650</span></h4>
+						  <h4>Guess Who? <span class="label label-warning"><?php $house = "Lords"; include "template/int-numberofmembers.php" ?></span></h4>
+						</a>
+					  </div>
+					   <div class="col-xs-6 col-md-3">
+						<a class="bootcards-summary-item" href="wrapups.php">
+						  <img src="favicons/android-chrome-192x192.png" height="64" width="64">
+						  <h4>Wind ups <span class="label label-success">650</span></h4>
 						</a>
 					  </div>
 					 </div>
@@ -55,11 +61,20 @@
 					?>
 					 <?php
 					 	// Can we get data from the Commons Oral Questions api?
-						$xmloral = @simplexml_load_file('http://lda.data.parliament.uk/commonsoralquestions.xml?_view=Commons+Oral+Questions&AnswerDate=2017-07-12&&_pageSize=1');
+						$xmloral = @simplexml_load_file('http://lda.data.parliament.uk/commonsoralquestions.xml?_view=basic&_pageSize=1');
 						if (!$xmloral) {
 							$oral = "danger";
 						} else { 
 							$oral = "success";
+						}
+					?>
+					<?php
+					 	// Can we get data from the AV Visual Live Logging api?
+						$xmlav = @simplexml_load_file('http://lda.data.parliament.uk/tvprogrammes.xml?_view=basic&_pageSize=1');
+						if (!$xmlav) {
+							$av = "danger";
+						} else { 
+							$av = "success";
 						}
 					?>
 					 <?php 
@@ -73,12 +88,13 @@
 					?>
 						  <h4><span class="label label-<?php echo $members; ?>">&nbsp</span> Members Data Platform</h4>
 						  <h4><span class="label label-<?php echo $oral; ?>">&nbsp</span> Commons Oral Questions</h4>
+						  <h4><span class="label label-<?php echo $av; ?>">&nbsp</span> Screenshots</h4>
 						  <h4><span class="label label-<?php echo $beta; ?>">&nbsp</span> Member Beta Images</h4>
 					  </div>
 					 </div>
 				  </div>
 				  <div class="panel-footer">
-					 <small>Data from UK Parliament - <a href="http://data.parliament.uk/membersdataplatform/">Members' Names Data Platform</a></small>
+					 <small>Data from UK Parliament - <a href="http://data.parliament.uk/">Data Dot Parliament (DDP)</a></small>
 				  </div>
 				</div>
 			</div>	

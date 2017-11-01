4871166d19d4c5c9efacce2559eb4eb08216f55a
Dan Leedham <danleedham@googlemail.com>
2017-11-01 17:00:02 +0000

    Manual Section Pick HoL Lists
    BAd baad bad way of doing this...

--- src/template/lordsquestions-sections.php
+++ src/template/lordsquestions-sections.php
@@ -8,21 +8,26 @@ $LordsUrl = "http://www.lordswhips.org.uk/todays-lists/";
 	// Each list of speakers starts with the list
 	$SplitOutSections = explode( '<p style="margin-left:36.0pt;"><strong>' , $content );
 	// Remove the waffle at the beginning of the page
-	$SplitOutSections = array_slice($SplitOutSections,1);	
-	
-	for($i=0; $i<count($SplitOutSections); $i++) {
-		if(strpos($SplitOutSections[$i],"<em>Speakers</em>")) {
-			$SplitOutSections[$i] = str_replace("followed by","",$SplitOutSections[$i]);
-			$SplitOutSections[$i] = explode("</strong></p>",$SplitOutSections[$i]);
-			$SplitOutSections[$i] = $SplitOutSections[$i][0];
-		} else {
-			$SplitOutSections[$i] = "";
+	if(count($SplitOutSections) == 1) {
+		$SplitOutSections = explode('<em>Main Business</em>',$content);
+		echo '<option value="0">List One</option><option value="1">List Two</option><option value="2">List Three</option>';
+	} else {
+		$SplitOutSections = array_slice($SplitOutSections,1);
+		
+		for($i=0; $i<count($SplitOutSections); $i++) {
+			if(strpos($SplitOutSections[$i],"<em>Speakers</em>")) {
+				$SplitOutSections[$i] = str_replace("followed by","",$SplitOutSections[$i]);
+				$SplitOutSections[$i] = explode("</strong></p>",$SplitOutSections[$i]);
+				$SplitOutSections[$i] = $SplitOutSections[$i][0];
+			} else {
+				$SplitOutSections[$i] = "";
+			}
 		}
-	}
-	
-	$SplitOutSections = array_values(array_filter($SplitOutSections));
+		
+		$SplitOutSections = array_values(array_filter($SplitOutSections));
 
-	for($i=0; $i<count($SplitOutSections); $i++) {
-		echo '<option value="'.$i.'">'.$SplitOutSections[$i].'</option>';
+		for($i=0; $i<count($SplitOutSections); $i++) {
+			echo '<option value="'.$i.'">'.$SplitOutSections[$i].'</option>';
+		}
 	}
 ?>

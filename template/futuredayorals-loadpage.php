e93dd735ca7ba687b80dc045603b31f9488a037e
Dan Leedham <danleedham@googlemail.com>
2017-12-31 13:48:21 +0000

    Give header to get file contents

--- template/futuredayorals-loadpage.php
+++ template/futuredayorals-loadpage.php
@@ -6,8 +6,15 @@
     */
     
 	// Load Future Orals URL
+	$context =  stream_context_create(
+	                array(
+	                    "http" =>   array(
+	                        "header" => "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+	                                )
+	                )
+	            );
 	$FOralsUrl = "https://publications.parliament.uk/pa/cm/cmfutoral/futoral.htm";
-	$FOralsContent = file_get_contents($FOralsUrl);
+	$FOralsContent = file_get_contents($FOralsUrl, false, $context);
 	
 	// Find the date the page was updated
 	$GetUpdatedDate = explode('<p class="Into_Intro">',$FOralsContent);
